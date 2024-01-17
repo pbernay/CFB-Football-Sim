cd3da6d8d5bc0924491f1dd2d39d398f90316a77
Paul Bernay <bernay.3@osu.edu>
2024-01-17 10:57:43 -0500

    Initial Generation for all essentials

--- data/intTeamsGen.py
+++ data/intTeamsGen.py
@@ -1,11 +1,17 @@
 import csv
 import random
+import os
 
 used_locations = set()
 
 
 def clearCSV():
-    with open("saveData\players.csv", "w"):
+    # Correct relative path from the parent directory of 'intPlayerGen.py'
+    directory = os.path.join("data", "saveData")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    # Correct the path here as well
+    with open(os.path.join(directory, "teams.csv"), "w"):
         pass
 
 
@@ -16,19 +22,28 @@ numberOfTeams = 261
 cities = []
 states = []
 
-with open("cities.txt", "r") as file:
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+rel_path = "cities.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+with open(abs_file_path, "r") as file:
     for line in file:
         city, state = line.strip().split(",")
         cities.append(city)
         cities.append(state)
 
-with open("teamNames.txt", "r") as file:
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+rel_path = "teamNames.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+with open(abs_file_path, "r") as file:
     teamNames = [name.strip() for name in file.readlines()]
 
-with open("cities.txt", "r") as file:
+script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+rel_path = "cities.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+with open(abs_file_path, "r") as file:
     locations = [city.strip() for city in file.readlines()]
 
-filePath = "saveData\\teams.csv"
+filePath = os.path.join("data", "saveData", "teams.csv")
 
 
 def uniqueLoc(locations):
