e793a99f06ea482c8405ed6f79b245890759a088
Paul Bernay <bernay.3@osu.edu>
2024-01-18 18:26:10 -0500

    Adding team lineup program to set team lineups on overall

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
 # helper functions or constants
+
+# Number of initial teams
 numberOfTeams = 261
 
+# Calculates the number of initial players to generate
 def calcNumberOfIntPlayers():
     numberOfIntPlayers = numberOfTeams * 64
     return numberOfIntPlayers
+
+# Global player count 
+globalPlayerCount = 0
+
+def incrementPlayerCount():
+    global globalPlayerCount
+    globalPlayerCount += 1
+    return globalPlayerCount
+def getPlayerCount():
+    return globalPlayerCount
